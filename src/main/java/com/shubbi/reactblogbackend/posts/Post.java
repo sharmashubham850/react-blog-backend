2424cccf2776b2605f3619375b1b57951a451f35
Shubham Sharma <sharmashubham850@gmail.com>
2022-02-04 19:21:57 +0530

    Implemented Postmodel and Post controller

--- src/main/java/com/shubbi/reactblogbackend/posts/Post.java
+++ src/main/java/com/shubbi/reactblogbackend/posts/Post.java
@@ -1,6 +1,22 @@
 package com.shubbi.reactblogbackend.posts;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String body;
@@ -9,45 +25,6 @@ public class Post {
     public Post(String title, String body, Integer userId) {
         this.title = title;
         this.body = body;
-        this.userId = userId;
-    }
-
-    public Post(Long id, String title, String body, Integer userId) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-        this.userId = userId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
         this.userId = userId;
     }
 }
--- src/main/java/com/shubbi/reactblogbackend/posts/PostController.java
+++ src/main/java/com/shubbi/reactblogbackend/posts/PostController.java
@@ -1,25 +1,69 @@
 package com.shubbi.reactblogbackend.posts;
 
+import com.shubbi.reactblogbackend.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class PostController {
 
-    @Autowired
-    private PostService postService;
+    private final PostRepository postRepository;
 
-    @GetMapping("")
-    List<PostEntity> getPosts(){
-        return postService.getAllPosts();
+    @Autowired
+    public PostController(PostRepository postRepository){
+        this.postRepository = postRepository;
     }
 
-    @GetMapping("{postId}")
-    PostEntity getPostById(@PathVariable("postId") Long postId){
-        return postService.getPostById(postId);
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> getPosts(){
+        List<Post> posts = this.postRepository.findAll();
+        return ResponseEntity.ok(posts);
+    }
+
+    @PostMapping("/post/new")
+    public ResponseEntity<Post> createPost(@RequestBody Post newPost){
+        Post createdPost = this.postRepository.save(newPost);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable("id") Long postId){
+        Post post = this.postRepository.findById(postId).orElseThrow(
+                ()-> new ResourceNotFoundException("Post with id "+ postId+ " not found")
+        );
+
+        return ResponseEntity.ok(post);
+    }
+
+    @PutMapping("/post/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable("id") Long postId, @RequestBody Post newPost){
+        return this.postRepository.findById(postId)
+                .map(post -> {
+                    post.setTitle(newPost.getTitle());
+                    post.setBody(newPost.getBody());
+                    post.setUserId(newPost.getUserId());
+                    Post savedPost = this.postRepository.save(post);
+                    return ResponseEntity.ok(savedPost);
+                })
+                .orElseGet(()->{
+                    newPost.setId(postId);
+                    Post savedPost = this.postRepository.save(newPost);
+                    return ResponseEntity.status(HttpStatus.CREATED).body(savedPost);
+                });
+    }
+
+    @DeleteMapping("/post/{id}")
+    public ResponseEntity<Post> deletePost(@PathVariable("id") Long postId){
+        Post post = this.postRepository.findById(postId).orElseThrow(
+                ()-> new ResourceNotFoundException("Post not found "+postId)
+        );
+
+        this.postRepository.delete(post);
+        return ResponseEntity.ok(post);
     }
 }
